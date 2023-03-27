487280cc16d436654e8903d2e85e01749eae91cf
andeha <andeha@users.noreply.github.com>
2023-03-27 16:19:45 +0200

    Improved exposition.

--- context-2.c
+++ context-2.c
@@ -1,19 +1,21 @@
 /* context-2.c | makecontext and swapcontext replacements originally from libtask. */
 
 #include "snapshot.h"
-#include <stdarg.h>
-#include <string.h>
+
+void * memset(void *b, int c, size_t len);
+
+#if defined __x86_64__
 
 void makecontext(struct ucontext *ucp, void (*func)(void), int argc, ...)
 {
    long *sp;
-   va_list va;
+   __builtin_va_list va;
    memset(&ucp->uc_mcontext, 0, sizeof ucp->uc_mcontext);
    /* if(argc != 2) *(int*)0 = 0; */
-   va_start(va, argc);
-   ucp->uc_mcontext.mc_rdi = (long)va_arg(va, void *);
+   __builtin_va_start(va, argc);
+   ucp->uc_mcontext.mc_rdi = (long)__builtin_va_arg(va, void *);
    /* ucp->uc_mcontext.mc_rsi = va_arg(va, int); */
-   va_end(va);
+   __builtin_va_end(va);
    sp = (long*)ucp->uc_stack.ss_sp+ucp->uc_stack.ss_size/sizeof(long);
    sp -= argc;
    sp = (void*)((uintptr_t)sp - (uintptr_t)sp%16);	/* 16-align for OS X */
@@ -21,6 +23,40 @@ void makecontext(struct ucontext *ucp, void (*func)(void), int argc, ...)
    ucp->uc_mcontext.mc_rip = (long)func;
    ucp->uc_mcontext.mc_rsp = (long)sp;
 }
+
+#elif defined __mips__
+
+void makecontext(ucontext_t *uc, void (*fn)(void), int argc, ...)
+{
+   int i, *sp;
+   __builtin_va_list arg;
+   
+   __builtin_va_start(arg, argc);
+   sp = (int*)uc->uc_stack.ss_sp+uc->uc_stack.ss_size/4;
+   for(i=0; i<4 && i<argc; i++)
+   	uc->uc_mcontext.mc_regs[i+4] = __builtin_va_arg(arg, int);
+   __builtin_va_end(arg);
+   uc->uc_mcontext.mc_regs[29] = (int)sp;
+   uc->uc_mcontext.mc_regs[31] = (int)fn;
+}
+
+#elif defined __armv8a__
+
+void makecontext(ucontext_t *uc, void (*fn)(void), int argc, ...)
+{
+   int i, *sp;
+   va_list arg;
+   
+   sp = (int*)uc->uc_stack.ss_sp+uc->uc_stack.ss_size/4;
+   __builtin_va_start(arg, argc);
+   for(i=0; i<4 && i<argc; i++)
+   	uc->uc_mcontext.gregs[i] = __builtin_va_arg(arg, uint);
+   __builtin_va_end(arg);
+   uc->uc_mcontext.gregs[13] = (uint)sp;
+   uc->uc_mcontext.gregs[14] = (uint)fn;
+}
+
+#endif
 
 int swapcontext(struct ucontext *oucp, const struct ucontext *ucp)
 {
